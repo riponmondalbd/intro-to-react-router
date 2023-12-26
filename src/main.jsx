77d3af38c36817b728feee495f9ac79a0c9b4578
Ripon Mondal <riponmondal149@gmail.com>
2023-12-26 22:36:18 +0600

    simple react router setup

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,25 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App></App>
+  },
+  {
+    path: '/about',
+    element: <div>This is the about page</div>
+  },
+  {
+    path: '/contact',
+    element: <div>Contact us now. Right now</div>
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>,
 )
