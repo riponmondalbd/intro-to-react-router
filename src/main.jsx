04406db5c1e86b0a666ac47563d86e7259ea906c
Ripon Mondal <riponmondal149@gmail.com>
2023-12-27 00:30:35 +0600

    nested route and outlet for shared content and avoid reload using link and load data on route

--- src/main.jsx
+++ src/main.jsx
@@ -3,19 +3,56 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import About from './components/About/About.jsx'
+import Contact from './components/Contact/Contact.jsx'
+import Error from './components/Error/Error.jsx'
+import Home from './components/Home/Home.jsx'
+import First from './components/First/First.jsx'
+import Friends from './components/Friends/Friends.jsx'
+
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <App></App>
+//   },
+//   {
+//     path: '/about',
+//     element: <About></About>
+//   },
+//   {
+//     path: '/contact',
+//     element: <Contact></Contact>
+//   },
+//   // {
+//   //   path: '/',
+//   //   element: <Root />,
+//   //   errorElement: <Error></Error>
+//   // }
+// ])
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <App></App>
-  },
-  {
-    path: '/about',
-    element: <div>This is the about page</div>
-  },
-  {
-    path: '/contact',
-    element: <div>Contact us now. Right now</div>
+    element: <Home></Home>,
+    children: [
+      {
+        path: '/',
+        element: <First></First>
+      },
+      {
+        path: 'friends',
+        element: <Friends></Friends>,
+        loader: () => fetch('https://jsonplaceholder.typicode.com/users')
+      },
+      {
+        path: 'about',
+        element: <About></About>
+      },
+      {
+        path: 'contact',
+        element: <Contact></Contact>
+      }
+    ]
   }
 ])
 
